d6e93ba3befb57815f167b598c4d1d74296597a4
dongthd <huudong297@gmail.com>
2026-08-02 21:11:20 +0700

    commit file

---
+++ src/main/java/com/springboot/fresher/SpringBootFresherApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.fresher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFresherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFresherApplication.class, args);
+	}
+
+}
